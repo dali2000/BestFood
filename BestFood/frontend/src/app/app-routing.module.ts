53aabff58a1dedb4c1db6f0b2a136c2977d833c8
ahmed stanbouli <ahmed.stambouli98@gmail.com>
2021-11-07 13:58:22 +0000

    add Register componet

--- BestFood/frontend/src/app/app-routing.module.ts
+++ BestFood/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistreComponent } from './client/registre/registre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:'Register',component:RegistreComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
