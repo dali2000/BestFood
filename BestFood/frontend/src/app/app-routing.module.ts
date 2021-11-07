717067fe61ac9fceba9aff840a990edacea17c0f
Amin Ben Othmen <aminos.bo.12@gmail.com>
2021-11-07 16:24:23 +0100

    creating restaurant component + routing

--- BestFood/frontend/src/app/app-routing.module.ts
+++ BestFood/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistreComponent } from './client/registre/registre.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
 const routes: Routes = [
- {path:'Register',component:RegistreComponent }
-
+ {path:'Register',component:RegistreComponent },
+ {path:'Restaurant',component:RestaurantComponent }
 ];
 
 @NgModule({
