83454d16df6ccae18140ab9866a9c2335d8a8b2b
dali2000 <medalikebair@gmail.com>
2021-11-07 16:23:43 +0100

    routerlink menu

--- BestFood/frontend/src/app/app-routing.module.ts
+++ BestFood/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistreComponent } from './client/registre/registre.component';
+import { MenuComponent } from './Menu/menu/menu.component';
 
 const routes: Routes = [
- {path:'Register',component:RegistreComponent }
+ {path:'Register',component:RegistreComponent },
+ {path:'Menu',component:MenuComponent}
 
 ];
 
