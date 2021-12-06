6bd3e60b88e96c205c12fafac910e48dcb03419a
dali2000 <medalikebair@gmail.com>
2021-12-06 21:16:12 +0100

    footer restaurent components

--- BestFood/frontend/src/app/app-routing.module.ts
+++ BestFood/frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistreComponent } from './client/registre/registre.component';
 import { MenuComponent } from './Menu/menu/menu.component';
+import {RestaurentComponent} from './restaurent/restaurent.component'
 
 const routes: Routes = [
  {path:'Register',component:RegistreComponent },
- {path:'Menu',component:MenuComponent}
+ {path:'Menu',component:MenuComponent},
+ {path:'Restaurent',component:RestaurentComponent},
 
 ];
 
--- BestFood/frontend/src/app/app.module.ts
+++ BestFood/frontend/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RegistreComponent } from './client/registre/registre.component';
 import { MenuComponent } from './Menu/menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RestaurentComponent } from './restaurent/restaurent.component';
 
 
 @NgModule({
@@ -14,6 +17,9 @@ import { MenuComponent } from './Menu/menu/menu.component';
     NavbarComponent,
     RegistreComponent,
     MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    RestaurentComponent,
     
   ],
   imports: [
