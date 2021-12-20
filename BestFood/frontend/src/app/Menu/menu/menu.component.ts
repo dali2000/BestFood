929d858b9d4d171f0a8e9e1d42cf2265fb780f12
dali2000 <medalikebair@gmail.com>
2021-12-20 13:07:12 +0100

    Menus

--- BestFood/frontend/src/app/Menu/menu/menu.component.ts
+++ BestFood/frontend/src/app/Menu/menu/menu.component.ts
@@ -11,5 +11,25 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  all = true;
+  dis = false;
+  die = false;
+  click(){
+    this.all =true;
+    this.die=false;
+    this.dis=false;
+    
+    
+  }
+  click1(){
+    this.all =false;
+    this.die=true;
+    this.dis=false;
+  }
+  click2(){
+    this.all =false;
+    this.die=false;
+    this.dis=true;
+  }
 
 }
