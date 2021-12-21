88f35f98d8781dfd94489b4677464cae500bc223
dali2000 <medalikebair@gmail.com>
2021-12-21 19:24:00 +0100

    ading login & profil api

--- BestFood/frontend/src/app/service/data-service.service.spec.ts
+++ BestFood/frontend/src/app/service/data-service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AppServiceService } from './app-service.service';
+import { DataServiceService } from './data-service.service';
 
-describe('AppServiceService', () => {
-  let service: AppServiceService;
+describe('DataServiceService', () => {
+  let service: DataServiceService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AppServiceService);
+    service = TestBed.inject(DataServiceService);
   });
 
   it('should be created', () => {
