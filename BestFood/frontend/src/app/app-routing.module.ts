0fd3b79020e59c203884960ce61fc6b95cd7a623
ahmed stanbouli <ahmed.stambouli98@gmail.com>
2021-12-21 13:37:33 +0100

    last update

--- BestFood/frontend/src/app/app-routing.module.ts
+++ BestFood/frontend/src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ import { DietFoodsComponent } from './diet-foods/diet-foods.component';
 import { DesertFoodsComponent } from './desert-foods/desert-foods.component';
 import { ProfilComponent } from './client/profil/profil.component';
 import { ContactComponent } from './contact/contact.component';
+import { UpdateProfilComponent } from './client/update-profil/update-profil.component';
+import { HistoriqueComponent } from './client/historique/historique.component';
+import { BasketComponent } from './basket/basket.component';
+
 
 
 const routes: Routes = [
@@ -16,13 +20,20 @@ const routes: Routes = [
  {path:'login',component:LoginComponent},
  {path:'contact',component:ContactComponent},
 
+ {path:'profil',component:ProfilComponent},
+  {path:'updateprofil',component:UpdateProfilComponent},
+  {path:'historique',component:HistoriqueComponent},
+
+  {path:'basket',component:BasketComponent},
+
+ 
+
  {path:'Menu',component:MenuComponent,children:[
   {path:'allProductes',component:AllProductesComponent},
   {path:'DietFoods',component:DietFoodsComponent},
   {path:'DesertFoods',component:DesertFoodsComponent},
  ]},
  {path:'Restaurent',component:RestaurentComponent},
- {path:'profil',component:ProfilComponent},
  {path:'Menu',component:MenuComponent},
  {path:'Restaurent',component:RestaurentComponent},
  {path:'Restaurant',component:RestaurentComponent}
