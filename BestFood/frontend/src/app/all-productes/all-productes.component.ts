4204f8bdf21015b7a87ce69ccd8775319f7c4b33
dali2000 <medalikebair@gmail.com>
2021-12-25 11:29:05 +0100

    update myMenu Restaurent

--- BestFood/frontend/src/app/all-productes/all-productes.component.ts
+++ BestFood/frontend/src/app/all-productes/all-productes.component.ts
@@ -17,6 +17,7 @@ export class AllProductesComponent implements OnInit {
   }
   data :any;
   foods:any;
+  
   n=0
 
  
--- BestFood/frontend/src/app/navbar/navbar.component.ts
+++ BestFood/frontend/src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import jwtDecode from 'jwt-decode';
@@ -13,18 +14,43 @@ export class NavbarComponent implements OnInit {
 
   imgPath = "../img/logo.png";
   url="/Register";
-  constructor( private service:DataServiceService, private router:Router) { }
+  constructor( private service:DataServiceService, private router:Router,private http: HttpClient) { }
   token : any;
   user : any;
   data :any;
+  data1:any;
+  n:any
+  public form = {
+    _id:null,
+    token:""
+  }
+
+  carts:any;
+  total=0
   ngOnInit(): void {
     this.token = localStorage.getItem('token');
     this.data = jwtDecode(this.token);
     this.user = this.data.user;
     console.log(this.user);
+    this.getbasket(this.form)
   }
   logout(){
     localStorage.removeItem('token');
     this.router.navigate(['/login'])
+  }
+  getbasket(form:any){
+    form.token = localStorage.getItem('token')
+    let headers = new HttpHeaders({'token': form.token})
+
+    console.log(headers)
+    this.http.get("http://localhost:3000/cart/cart",{headers:headers}).subscribe(res=>{
+      this.data1 = res;
+      this.n = this.data1.cart.length;
+      
+
+
+
+    })
+
   }
 }
