0527e8c08833444a305dab25b955c3742c00fd66
Amin-Bo <aminos.bo.12@gmail.com>
2021-12-26 14:35:44 +0100

    added admin homePage users and resto interfaces

--- BestFood/frontend/src/app/app.module.ts
+++ BestFood/frontend/src/app/app.module.ts
@@ -31,6 +31,10 @@ import { MyMenuComponent } from './my-menu/my-menu.component';
 import { UpdateRestoComponent } from './restaurent/update-resto/update-resto.component';
 import { ContasComponent } from './admin/contas/contas.component';
 import { SideBarAdminComponent } from './admin/side-bar-admin/side-bar-admin.component';
+import { HommeComponent } from './admin/homme/homme.component';
+import { ProfilAdminComponent } from './admin/profil-admin/profil-admin.component';
+import { RestoComponent } from './admin/resto/resto.component';
+import { UsersComponent } from './admin/users/users.component';
 
 
 
@@ -68,6 +72,10 @@ import { SideBarAdminComponent } from './admin/side-bar-admin/side-bar-admin.com
     UpdateRestoComponent,
     ContasComponent,
     SideBarAdminComponent,
+    HommeComponent,
+    ProfilAdminComponent,
+    RestoComponent,
+    UsersComponent,
 
 
 
