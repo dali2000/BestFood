06c4ef29196436f418498555f4992591570dc24f
Amin-Bo <aminos.bo.12@gmail.com>
2021-12-27 22:27:30 +0100

    added admin profile info

--- BestFood/frontend/src/app/admin/profil-admin/profil-admin.component.ts
+++ BestFood/frontend/src/app/admin/profil-admin/profil-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import jwtDecode from 'jwt-decode';
+import { DataServiceService } from 'src/app/service/data-service.service';
 
 @Component({
   selector: 'app-profil-admin',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilAdminComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service:DataServiceService, private router:Router) { }
+  token : any;
+  user : any;
+  data :any;
   ngOnInit(): void {
+    this.token = localStorage.getItem('token');
+    this.data = jwtDecode(this.token);
+    this.user = this.data.user;
+    console.log(this.user);
+
   }
 
 }
