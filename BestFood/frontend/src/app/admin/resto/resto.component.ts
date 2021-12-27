78eee813edb0228fd67eb94051db8c720a18b5f8
Amin-Bo <aminos.bo.12@gmail.com>
2021-12-27 22:39:53 +0100

    get all restaurants

--- BestFood/frontend/src/app/admin/resto/resto.component.ts
+++ BestFood/frontend/src/app/admin/resto/resto.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-resto',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.getResto()
+  }
+  data:any;
+  restos:any;
+
+  getResto(){
+    this.http.get("http://localhost:3000/resto/restaurants").subscribe(res =>{
+
+      this.data = res;
+     this.restos =this.data.restaurants;
+     console.log(this.restos);
+    })
   }
 
 }
